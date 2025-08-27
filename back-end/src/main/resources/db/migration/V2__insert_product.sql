06729a22c4cf70dbdf0dd0d126cc0fe1459a3ca7
olegnik <olegnik11@ukr.net>
2025-08-27 22:33:41 +0300

    V2_insert in data

---
+++ back-end/src/main/resources/db/migration/V2__insert_product.sql
@@ -0,0 +1,4 @@
+INSERT INTO catalogue.t_product (c_title, c_price, c_details) VALUES
+     ('Coffee Beans 250g', 899, 'Arabica blend'),
+     ('Ceramic Mug 300ml', 499, 'Dishwasher safe'),
+     ('Reusable Bottle 0.5L', 1499, 'Stainless steel');
