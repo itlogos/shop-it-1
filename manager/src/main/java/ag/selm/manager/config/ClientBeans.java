22c8e227d113a97106deb82bedaef4915856147b
olegnik <olegnik11@ukr.net>
2025-09-05 12:18:51 +0300

    try security (but don't work), Big complect.

--- manager/src/main/java/ag/selm/manager/config/ClientBeans.java
+++ manager/src/main/java/ag/selm/manager/config/ClientBeans.java
@@ -1,9 +1,11 @@
 package ag.selm.manager.config;
 
 import ag.selm.manager.client.RestClientProductsRestClient;
+import ag.selm.manager.security.OAuthClientHttpRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,9 +13,15 @@ public class ClientBeans {
 
     @Bean
     public RestClientProductsRestClient productsRestClient(
-            @Value("${selmag.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUri) {
-        return new RestClientProductsRestClient(RestClient.builder()
-                .baseUrl(catalogueBaseUri)
-                .build());
+            @Value("${selmag.service.catalogue.uri:http://back:8081}") String baseUri,
+            OAuth2AuthorizedClientManager authorizedClientManager) {
+
+        return new RestClientProductsRestClient(
+                RestClient.builder()
+                        .baseUrl(baseUri)
+                        .requestInterceptor(
+                                new OAuthClientHttpRequestInterceptor(authorizedClientManager, "keycloak"))
+                        .build()
+        );
     }
 }
